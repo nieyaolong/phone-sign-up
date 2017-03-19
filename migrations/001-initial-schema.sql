573b33eefcc90a39e193987458922633792db763
nieyaolong@gmail.com <nie753951>
2017-03-20 01:36:42 +0800

    persistence

---
+++ migrations/001-initial-schema.sql
@@ -0,0 +1,5 @@
+-- Up
+CREATE TABLE USER (name TEXT, phone TEXT, major TEXT, score TEXT, timestamp TIMESTAMP);
+
+-- Down
+DROP TABLE USER;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const svgCaptcha = require('svg-captcha');
+const db = require('sqlite');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     let captcha = svgCaptcha.create();
     req.session.captcha = captcha.text;
 
@@ -13,39 +14,49 @@ router.get('/', function(req, res, next) {
 });
 
 function signUpAsync(info) {
-    return new Promise((resolve, reject) => {
-        console.error(info);
-        return resolve();
-    });
+    let dbFile = './db.sqlite';
+    return db.open(dbFile)
+        .then(() => {
+            return db.migrate();
+        })
+        .then(() => {
+            return db.run(`INSERT INTO USER (name, phone, major, score) VALUES ("${info.name}", "${info.phone}", "${info.major}", "${info.score}")`);
+        })
+        .then(() => {
+            db.close();
+        });
 }
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     let phone = req.body.phone;
     let name = req.body.name;
     let code = req.body.code;
+    let major = req.body.major;
+    let score = req.body.score;
 
     let sessionPhone = req.session.phone;
     let sessionCode = req.session.code;
 
-    if(!name) {
+    if (!name) {
         res.status(400).send('Bad Name');
         return;
     }
 
 
-    if(!(phone && sessionPhone && phone === sessionPhone)) {
+    if (!(phone && sessionPhone && phone === sessionPhone)) {
         res.status(400).send('Bad Phone');
         return;
     }
 
-    console.error(`${code}, ${sessionCode}`);
-    if(!(code && sessionCode && code === sessionCode)) {
+    if (!(code && sessionCode && code === sessionCode)) {
         res.status(400).send('Bad Code');
         return;
     }
     let regInfo = {
         name: name,
         phone: phone,
+        major: major,
+        score: score
     };
     signUpAsync(regInfo)
         .then(() => {
